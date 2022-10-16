269ab794fc05752dec9ef69f98698fe94886517b
EllieKu <eli52583@hotmail.com>
2022-10-16 18:48:20 +0800

    init

---
+++ src/context.js
@@ -0,0 +1,12 @@
+// import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+const RoomContext = React.createContext()
+
+export default class RoomProvider extends Component {
+  render() {
+    return (
+      <div>context</div>
+    )
+  }
+}

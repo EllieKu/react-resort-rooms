153b0b0a07e7bd0bd00252abc2784b008d9b6d0e
EllieKu <eli52583@hotmail.com>
2022-10-20 22:17:40 +0800

    feat: context and page FeaturedRooms

--- src/context.js
+++ src/context.js
@@ -3,10 +3,20 @@ import React, { Component } from 'react'
 
 const RoomContext = React.createContext()
 
-export default class RoomProvider extends Component {
+class RoomProvider extends Component {
+  state = {
+    greeting: 'hello',
+    name: 'bubu'
+  }
   render() {
     return (
-      <div>context</div>
+      <RoomContext.Provider value={{ ...this.state }}>
+        {this.props.children}
+      </RoomContext.Provider>
     )
   }
 }
+
+const RoomConsumer = RoomContext.Consumer
+
+export { RoomProvider, RoomConsumer, RoomContext }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter  } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RoomProvider } from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RoomProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RoomProvider>
   </React.StrictMode>
 );
 
